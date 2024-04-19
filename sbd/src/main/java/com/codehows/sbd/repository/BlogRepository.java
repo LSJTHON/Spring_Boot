198cceddc9b673c46c4ec8918310a9e666b74d55
이성종 <104560357+LSJTHON@users.noreply.github.com>
2024-04-19 17:06:20 +0900

    Delete sbd directory

--- sbd/src/main/java/com/codehows/sbd/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codehows.sbd.repository;
-
-import com.codehows.sbd.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article,Long> {
-
-}
